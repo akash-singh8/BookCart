55a3e8b8710226d9749c1cfece2cc1b18da28c6b
akash-singh8 <developer.akash8@gmail.com>
2023-05-04 12:11:22 +0530

    Update seller verification to DB

--- src/components/Seller.js
+++ src/components/Seller.js
@@ -1,28 +1,81 @@
 import React from "react";
 import "./CSS/seller.css";
-import sellerStore from "./images/sellerStore.png";
-import smartphone from "./images/smartphone.png";
+import sellerStore from "./images/Seller/sellerStore.png";
+import telegram from "./images/Seller/Telegram.png";
+import location from "./images/Seller/locationIcon.png";
+import { db } from "../firebase";
+import { NavLink } from "react-router-dom";
 
-function Seller() {
+function Seller({ verified, setVerified, user }) {
+  function becomeSeller() {
+    if (user) {
+      const sellerDetails = document.querySelectorAll(
+        ".sellerDetailInput input"
+      );
+      const telegram = sellerDetails[0];
+      const location = sellerDetails[1];
+
+      db.collection("users").doc(user).update({
+        verified: true,
+        telegram: telegram.value,
+        location: location.value,
+        bookToSell: [],
+      });
+      setVerified(true);
+      telegram.value = "";
+      location.value = "";
+    } else {
+      alert("Please Sign In to proceed");
+    }
+  }
   return (
     <section id="home">
-      <div id="details">
+      <form
+        id="details"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
         <h1>SELL BOOKS</h1>
         <h2 className="gradient_head">MAKE YOUR BOOK ALIVE AGAIN</h2>
         <div id="sellerInput">
-          <p>Verify to become a seller</p>
-          <div id="sellerPhoneInput">
-            <img src={smartphone} alt="smartphone" width={28} />
-            <input
-              type="number"
-              placeholder="Enter 10 digit phone number here"
-            />
-          </div>
+          <p style={{ marginBottom: "22px" }}>
+            {verified ? "Verified Seller" : "Fill details to become a seller"}
+          </p>
+          {verified ? (
+            <div>Seller ID: {user}</div>
+          ) : (
+            <div>
+              <div className="sellerDetailInput">
+                <img src={telegram} alt="telegram" />
+                <input
+                  type="text"
+                  placeholder="Enter your telegram id here"
+                  required
+                />
+              </div>
+              <div className="sellerDetailInput">
+                <img src={location} alt="location" />
+                <input
+                  type="text"
+                  placeholder="Enter your location to sell books"
+                  required
+                />
+              </div>
+            </div>
+          )}
         </div>
-        <div id="detailButtons">
-          <button>Get OTP</button>
+        <div className="detailButtons">
+          {verified ? (
+            <NavLink to="verified">
+              <button>Upload New Book</button>
+            </NavLink>
+          ) : (
+            <button type="submit" onClick={becomeSeller}>
+              Become Seller
+            </button>
+          )}
         </div>
-      </div>
+      </form>
       <img src={sellerStore} alt="online store" width={600} />
     </section>
   );
